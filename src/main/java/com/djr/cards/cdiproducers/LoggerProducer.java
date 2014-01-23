0b7085320960c90f9d1110c5c9399fca17974ef0
djr4488 <djr4488@gmail.com>
2014-01-23 08:37:29 -0600

    Made logging configurable

--- src/main/java/com/djr/cards/cdiproducers/LoggerProducer.java
+++ src/main/java/com/djr/cards/cdiproducers/LoggerProducer.java
@@ -1,9 +1,16 @@
 package com.djr.cards.cdiproducers;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Default;
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
+import javax.inject.Inject;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
+import ch.qos.logback.core.util.StatusPrinter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,9 +24,24 @@ import org.slf4j.LoggerFactory;
 @Default
 public class LoggerProducer {
     private static final Logger log = LoggerFactory.getLogger(LoggerProducer.class);
+    public LoggerProducer() {}
 
     @Produces
     public Logger getLogger(InjectionPoint ip) {
+        // assume SLF4J is bound to logback in the current environment
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        try {
+            JoranConfigurator configurator = new JoranConfigurator();
+            configurator.setContext(context);
+            // Call context.reset() to clear any previous configuration, e.g. default
+            // configuration. For multi-step configuration, omit calling context.reset().
+            context.reset();
+            configurator.doConfigure("/app/cards/conf/logback.xml");
+        } catch (JoranException je) {
+            // StatusPrinter will handle this
+        }
+        StatusPrinter.printInCaseOfErrorsOrWarnings(context);
         Class<?> injectingClass = ip.getMember().getDeclaringClass();
         log.debug("getLogger() injectingClass:{}", injectingClass.getName());
         return LoggerFactory.getLogger(injectingClass);
