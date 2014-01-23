def867f2cc91faaf0e43908f685e7991e06d1afc
djr4488 <djr4488@gmail.com>
2014-01-23 08:51:52 -0600

    Adding test cases

---
+++ src/test/java/com/djr/auth/AuthServiceTest.java
@@ -0,0 +1,13 @@
+package com.djr.auth;
+
+import junit.framework.TestCase;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: djr4488
+ * Date: 1/23/14
+ * Time: 8:43 AM
+ */
+public class AuthServiceTest extends TestCase {
+
+}
