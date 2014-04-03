8fbce3a889dbb0f50d7c96cf43208d3739b81815
djr4488 <djr4488@gmail.com>
2014-04-02 22:15:39 -0500

    Added the initial join game rest method

--- src/main/java/com/djr/cards/games/restapi/GameController.java
+++ src/main/java/com/djr/cards/games/restapi/GameController.java
@@ -6,6 +6,7 @@ import com.djr.cards.games.GameService;
 import com.djr.cards.games.models.CreateGameResult;
 import com.djr.cards.games.models.GameModel;
 import com.djr.cards.games.models.GameResponse;
+import com.djr.cards.games.models.JoinGameResult;
 import org.slf4j.Logger;
 
 import javax.inject.Inject;
@@ -56,6 +57,45 @@ public abstract class GameController extends BaseGameController {
             return gameResponse;
         }
         removeAndSetSessionAttribute("gameid", createGameResult.game.getId(), request);
+        gameResponse.nextLanding = "golfGame";
+        return gameResponse;
+    }
+
+    private String joinGameResultInlineError(JoinGameResult joinGameResult) {
+        if (joinGameResult.landingAction.equals("inlinePassword")) {
+            return "That was the wrong password for the game.";
+        }
+        if (joinGameResult.landingAction.equals("inlineStarted")) {
+            return "Just a bit slow since the game already started.";
+        }
+        if (joinGameResult.landingAction.equals("inlineTriesExceeded")) {
+            return "I couldn't seem to get you into the game, I did try though.";
+        }
+        if (joinGameResult.landingAction.equals("inlineAlreadyJoined")) {
+            return "It is a most odd conundrum, you seem to be in this game already.";
+        }
+        if (joinGameResult.landingAction.equals("inlineNotFound")) {
+            return "I didn't find this game, you sure this is the game?";
+        }
+        return null;
+    }
+
+    @POST
+    @Path("/join/submit")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public GameResponse joinGame(GameModel gameModel, @Context HttpServletRequest request) {
+        String tracking = (String)getSession(request).getAttribute("tracking");
+        log.info("joinGame() - gameModel:{}, tracking:{}", gameModel, tracking);
+        GameResponse gameResponse = new GameResponse();
+        User user = (User)getSession(request).getAttribute("user");
+        JoinGameResult joinGameResult = gameService.joinGame(gameModel, user, tracking);
+        String inlineError = joinGameResultInlineError(joinGameResult);
+        if (inlineError != null) {
+            gameResponse.errorBold = "Whoa there!";
+            gameResponse.errorMsg = inlineError;
+            return gameResponse;
+        }
         gameResponse.nextLanding = "golfGame";
         return gameResponse;
     }
